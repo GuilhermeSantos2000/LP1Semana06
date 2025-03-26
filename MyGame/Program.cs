bbbb95c6cc343f600bf61edea75080fc86669836
GuilhermeSantos2000 <guilhermesantos.gamedev@gmail.com>
2025-03-26 22:41:29 +0000

    add enemy handler to MyGame class

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace MyGame
 {
@@ -6,6 +7,20 @@ namespace MyGame
     {
         private static void Main(string[] args)
         {
+            int NumberOfEnemies = int.Parse(args[0]);
+            Enemy[] enemies = new Enemy[NumberOfEnemies];
+
+            for (int i = 0; i < NumberOfEnemies; i++)
+            {
+                Console.Write($"Nome do inimigo {i + 1}: ");
+                string enemyName = Console.ReadLine();
+                enemies[i] = new Enemy(enemyName);
+            }
+
+            foreach (Enemy enemy in enemies)
+            {
+                Console.WriteLine($"{enemy.GetName()} {enemy.GetHealth()} {enemy.GetShield()}");
+            }
         }
     }
 }
