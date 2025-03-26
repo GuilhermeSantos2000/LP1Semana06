ffef9a93a79e2c5ccec93012ec807ff01f1a7bdb
GuilhermeSantos2000 <guilhermesantos.gamedev@gmail.com>
2025-03-26 20:08:44 +0000

    Now we can roll the dice

--- RandDice/Program.cs
+++ RandDice/Program.cs
@@ -6,7 +6,16 @@ namespace RandDice
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            int n = int.Parse(args[0]);
+            int s = int.Parse(args[1]);
+
+            int sum = 0;
+
+            Random dice = new Random(s);
+            for (int i = 0; i < n; i++)
+               sum += dice.Next(1,7);
+
+            Console.WriteLine(sum);
         }
     }
 }
