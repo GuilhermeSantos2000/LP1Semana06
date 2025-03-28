cea758e2a7fb8f444c0143b64921c3f6f25c5f50
GuilhermeSantos2000 <guilhermesantos.gamedev@gmail.com>
2025-03-28 17:08:50 +0000

    add getters and room array

---
+++ MyRoguelike/Level.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace MyRoguelike
+{
+    public class Level
+    {
+        private int numberOfRooms;
+        private Toughness difficulty;
+        private Enemy[] enemiesInRooms;
+
+        public Level(int numberOfRooms, Toughness difficulty)
+        {
+            this.numberOfRooms = numberOfRooms;
+            this.difficulty = difficulty;
+        }
+
+        public void SetEnemyInRoom(int roomIndex, Enemy enemy)
+        {
+            enemiesInRooms[roomIndex] = enemy;
+        }
+
+        public int GetNumRooms()
+        {
+            return numberOfRooms;
+        }
+
+        public Toughness GetToughness()
+        {
+            return difficulty;
+        }
+
+        public int GetNumEnemies()
+        {
+            int numEnemies = 0;
+            for (int i = 0; i < numberOfRooms; i++)
+            {
+                if (enemiesInRooms[i] != null)
+                {
+                    numEnemies++;
+                }
+            }
+            return numEnemies;
+        }
+        
+    }
+}
